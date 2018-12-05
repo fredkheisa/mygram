6c1a0f53a1d1f80c48272e27a6249b77ae0a2b70
Github User Name <you@example.com>
2018-12-05 12:48:32 +0300

    extended the app with both bootstrap3 and bootstrap4

--- gram/settings.py
+++ gram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news.apps.NewsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
