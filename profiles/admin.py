16dd83ef8de39abb2ae1855b60a92e73fa72707a
Github User Name <you@example.com>
2018-12-06 12:23:25 +0300

    creating the django super user or admin at this commit

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Editor,Article,tags
 
 # Register your models here.
+admin.site.register(Editor)
+admin.site.register(PICTURES)
+admin.site.register(tags)
